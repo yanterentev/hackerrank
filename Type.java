fdc4e6ba391dfa8eee5470723f3172c07b99815d
yanterentev <yanterentev@gmail.com>
2017-11-19 15:20:47 +0200

    java types (all primitive types)

---
+++ Type.java
@@ -0,0 +1,23 @@
+
+
+public class Type {
+	public static void main(String[] args) {
+		boolean trueOrFalse = true; Boolean b = true;
+		trueOrFalse = false;
+		String answer = args.length > 0 ? "yes" : "no";
+		//String answer;
+		//if (args.length > 0) {
+		//	answer = "yes";
+		//} else {
+		//	answer = "no";
+		//}
+
+		char ch = 'c'; Character charr;
+		short sh = 12; Short ssh;
+		int in = 123_123; Integer iiin; 
+		long asd = 1_212_300L; Long llll;
+
+		float f = .0f; Float fffff;
+		double d = 1_000_000.0; Double dddd;
+	}
+}
